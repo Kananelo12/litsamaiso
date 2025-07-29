485533dfd0bbbfed0b6a92000c9e5d77e58b918b
Kananelo12 <kananeloj12@gmail.com>
2025-07-29 08:26:19 +0200

    Manually move Header component into feature-auth branch

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import { getCurrentUser } from '@/utils/actions/auth.action'
 import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react'
@@ -6,7 +7,10 @@ const layout = async ({ children }: { children: ReactNode }) => {
   const user = await getCurrentUser();
   if (!user) redirect("/sign-in");
   return (
-    <div>{children}</div>
+    <div>
+      <Header username='Joel' />
+      {children}
+      </div>
   )
 }
 
