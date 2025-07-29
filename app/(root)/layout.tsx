da6b820d7f2a1ae54ee4642ea543c32eb725fb8e
Kananelo12 <kananeloj12@gmail.com>
2025-07-29 09:43:33 +0200

    Fix peerDependency issue by removing ts-node-esm and ts-node

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,18 @@
-import Header from '@/components/Header';
-import { getCurrentUser } from '@/utils/actions/auth.action'
-import { redirect } from 'next/navigation';
-import React, { ReactNode } from 'react'
+import Header from "@/components/Header";
+import { getCurrentUser } from "@/utils/actions/auth.action";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const user = await getCurrentUser();
   if (!user) redirect("/sign-in");
+
   return (
     <div>
-      <Header username='Joel' />
+      <Header username={user.name} />
       {children}
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default layout
+export default layout;
